94a16f7789e03c2dfc51aff45ca33f1a28151794
Min Joon Choi <agarkorea@gmail.com>
2021-05-04 12:15:21 +0900

    Add files via upload

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool isJump = false;
+    bool isTop = false;
+    public float jumpHeight = 0;
+    public float jumpSpeed = 0;
+
+    Vector2 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            isJump = true;  
+		}else if(transform.position.y <= startPosition.y){
+            isJump = false;
+            isTop = false;
+            transform.position = startPosition;
+		}
+
+        if(isJump){
+            if(transform.position.y <= jumpHeight - 0.1f && !isTop){
+              transform.position = Vector2.Lerp(transform.position, 
+              new Vector2(transform.position.x, jumpHeight), jumpSpeed*Time.deltaTime);
+            }else{
+                isTop = true;     
+			}
+		}
+
+        if(transform.position.y > startPosition.y && isTop){
+              transform.position = Vector2.MoveTowards(transform.position, startPosition, jumpSpeed * Time.deltaTime);
+		}
+    }
+}
